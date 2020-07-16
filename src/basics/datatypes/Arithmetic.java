94542458708a57b5a1416c95ecc2950788d67ab1
Muralidhar Reddy Kotakonda <muralidhar.reddy.kotakonda@sap.com>
2020-07-17 01:15:06 +0530

    updated data

---
+++ src/basics/datatypes/Arithmetic.java
@@ -0,0 +1,77 @@
+package basics.datatypes;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		//arithmetic operations
+		int a= 60;
+		int b = 20;
+		
+	    int sum  = a+b;
+	    int sub =a-b;
+	    int mul = a*b;
+	    int div = a/b;
+	    int mod = a%b;
+	    
+	    System.out.println(sum);
+	    System.out.println(sub);
+	    System.out.println(mul);
+	    System.out.println(div);
+	    System.out.println(mod);
+	    
+	    
+	    //assignment
+	    int z= a; // assign the valu of a to z
+	    System.out.println("z="+z);
+	    
+	    
+	    //incrementor , decrementor
+	    int x= 90;
+	    
+	    x++; //x = x+1 post increment
+	    ++x; //x = x+1 pre increment
+	    
+	    System.out.println(x++);
+	    System.out.println(++x);
+	    
+	 
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    //assignment
+	    x = x+5; // or x+= 5;
+	    x = x-5; // or x-= 5;
+	    x = x*5; // or x*= 5;
+	    x = x/5; // or x/= 5;
+	    
+	    //bitwise operators &,|,~ ,^ , << , >>	    
+        int v1= 5;  //     0101   
+        int v2=1;   //     0001
+        
+        System.out.println(v1&v2);  // bit value will be 1 if both bits are 1 [ 0001 ]
+        System.out.println(v1|v2); // bit value will be 1 if any bit is 1 [ 0101 ]
+        int v3= ~v2;
+        System.out.println(v3);    // 0101   ----> 1010  
+	    		
+	    
+	    
+	    
+	    
+	}
+}
--- src/basics/datatypes/DataTypes.java
+++ src/basics/datatypes/DataTypes.java
@@ -2,7 +2,34 @@ package basics.datatypes;
 
 public class DataTypes {
 public static void main(String[] args) {
-	/*byte:
+	/*
+	//variables : hold the real time static/dynamic data
+		//memory allocation/processing for the data depends on tyep of data ..
+		
+		
+		//datatypes : [type of the data ]. EVERY Variable is created by specifying the datatype.
+		
+		/*
+		 datatype           size (in bytes)
+		 ----------------------------------------------  
+		 byte               1    ( byte is the DT that represents any Data type )
+		                          when we need to communicate with third party.
+		 						ex: DB, FILE, WEB etc...
+		 
+		 short              2      -32768 to 32767                                    
+		 int                4      number
+		 long               8      bigger number
+		 
+		 
+		 float              4      -> decimal
+		 double             8      -> bigger decimal
+		 
+		 char               2           
+		 String             depends on no of chars
+		 
+		 boolean            1 bit    -> true/false  
+	
+	byte:
 		This can hold whole number between -128 and 127. 
 	Default size of this data type: 1 byte.	
 	
@@ -31,7 +58,47 @@ public static void main(String[] args) {
 	float: Sufficient for holding 6 to 7 decimal digits
 		size: 4 bytes
 		
+		// how to create a varaible datatype + variable name
+		
+		
+		rules for variable names
+		1.[a-z[A-Z][0-9] [ _ $ ]
+		2.start with alphabet or _ or $ , but not with number. 
+		cannot use the java reserved keywords 
 	
 			*/
+		short a = 23; // a is a varibale of short data type with 23 a s value
+		// declaration of variable + assignment of value
+
+		int b; // declaration of variable
+		b = 1313; // assign the values
+
+		long c = 9052424242422424242L;
+		float d = 89.1313131f;
+		double e = 24242424422424.24242424242424d;
+
+		char f = '6'; // 1 single char [0-9] [a-z] [ A-Z] [!@#$%^&*()<>?:"{}....]
+		String g = "my name is krishna. iam a java developer@ 1234 ";
+
+		boolean h = false;
+		boolean m = true;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(m);
+
+		// print all varible values in one line seperated by comma
+		String joinData = a + " ," + b + " , " + c;
+		System.out.println(joinData);// print all varibale values in line
+
+		// o/p:value of a = 23 , value of b = 1313 , value of c = 9052424242422424242L
+		// ..etc........................
+
 }
 }
